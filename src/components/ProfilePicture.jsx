3d9a25f6607a0b2aa1ea4c813135a52ae4f34bf5
Rajpal@123 <you@example.com>
2023-10-17 17:47:42 +0530

    profilePicture

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { stables } from '../constants'
 import { HiOutlineCamera } from 'react-icons/hi'
+import CropEasy from './ImageCrop/CropEasy';
+import { createPortal } from 'react-dom';
+
+
 const ProfilePicture = ({ avatar }) => {
+    const [openCrop, setOpenCrop] = useState(false);
+    const [photo, setPhoto] = useState(null);
+
+    const handleChangeFile = (e) => {
+        const file = e.target.files[0];
+        setPhoto({ url: URL.createObjectURL(file), file });
+        setOpenCrop(true);
+    }
+
     return (
-        <div className=' w-full flex items-center gap-x-4'>
-            <div className=' relative w-20 h-20 rounded-full outline
+        <>
+            {
+                openCrop && createPortal(<CropEasy photo={photo}
+                    setOpenCrop={setOpenCrop}
+                />, document.getElementById("portal"))
+            }
+            <div className=' w-full flex items-center gap-x-4'>
+                <div className=' relative w-20 h-20 rounded-full outline
              outline-offset-2 outline-1 outline-primary overflow-hidden'>
-                <label htmlFor='profilePicture' className=' cursor-pointer bg-transparent
+                    <label htmlFor='profilePicture' className=' cursor-pointer bg-transparent
                  absolute inset-0 rounded-full'>
-                    {
-                        avatar ? (
-                            <img src={stables.UPLOAD_FOLDER_BASE_URL + avatar}
-                                className=' w-full h-full object-cover'
-                                alt="profile pic" />
-                        ) : (
-                            <div className=' w-full h-full bg-blue-50 flex justify-center items-center'>
-                                <HiOutlineCamera />
-                            </div>
-                        )
-                    }
-                </label>
-                <input type="file" className=' sr-only' id='profilePicture' />
+                        {
+                            avatar ? (
+                                <img src={stables.UPLOAD_FOLDER_BASE_URL + avatar}
+                                    className=' w-full h-full object-cover'
+                                    alt="profile pic" />
+                            ) : (
+                                <div className=' w-full h-full bg-blue-50 flex justify-center items-center'>
+                                    <HiOutlineCamera />
+                                </div>
+                            )
+                        }
+                    </label>
+                    <input type="file" className=' sr-only' id='profilePicture'
+                        onChange={handleChangeFile}
+                    />
+                </div>
+                <button type='button' className="border border-red-500 rounded-lg px-4 py-2 text-red-500">
+                    Delete
+                </button>
             </div>
-            <button type='button' className="border border-red-500 rounded-lg px-4 py-2 text-red-500">
-                Delete
-            </button>
-        </div>
+        </>
     )
 }
 
--- src/services/index/users.js
+++ src/services/index/users.js
@@ -73,3 +73,23 @@ export const updateProfile = async ({ token, userData }) => {
     throw new Error(error.message);
   }
 };
+export const updateProfilePicture = async ({ token, formData }) => {
+  try {
+    const config = {
+      headers: {
+        "Content-type":"multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data } = await axios.put(
+      "https://first-blog-website.cyclic.app/api/user/updateProfilePicture",
+      formData,
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
